d49d3884aaa7ab8945ad1c4d49862441776afb8c
anandmanic <manand2705@gmail.com>
2024-10-17 12:43:51 +0530

    UPLOAD

---
+++ Java.js
@@ -0,0 +1,52 @@
+var a="Hello"
+var b="World"
+var c="Program"
+var l=20,m=10,n=5;
+var x,y,z;
+var length,width,height;
+
+function add(a,b){
+    console.log("Value of A:",a,"Value of B:",b);
+    return a+b;
+}
+var result=add(3,5);
+console.log("Value after Addition:",result);
+
+// function list(){
+//     console.log("Output: "+a,b,c)
+// }
+
+// function add(){
+//     console.log("Addition      :",l+m)
+// }
+
+// function sub(){
+//     console.log("Subtraction   :",m-n)
+// }
+
+// function mul(){
+//     console.log("Multiplication:",l*n)
+// }
+// function div(){
+//     console.log("Division      :",l/m)
+// }
+
+// function Arith(x,y,z){
+//     console.log(":::: Arithmetic Functions ::::")
+//     console.log("Add:",x+y)
+//     console.log("Sub:",x-y)
+//     console.log("Mul:",x*z)
+//     console.log("Div:",x/z)
+// }
+// function area(length,width,height){
+//     console.log("~>Area of Cube<~")
+//     console.log("Volume:",length*width*height)
+// }
+
+// list()
+// add()
+// sub()
+// mul()
+// div()
+// Arith(20,10,5)
+// area(10,8,5)
